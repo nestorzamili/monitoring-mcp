4c8c55785f81c4f06c963612b7404aa3aa94d38b
Nestor <nestorzamili@gmail.com>
2024-09-24 12:15:52 +0700

    Refactor perencanaan.tsx: Remove redundant Badge import

--- app/perencanaan/perencanaan.tsx
+++ app/perencanaan/perencanaan.tsx
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 
